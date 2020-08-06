93e90deafc6a2e255423fa189b76f3eea06f9da4
Jacob Stavrianos <jacobstavrianos@gmail.com>
2020-08-06 09:45:57 -0400

    added readme info, minor changes to main.js

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # commitment-bot
 a discord bot for commitments; lets you record time commitments and holds you to them later
+
+# dependencies
+Require node.js (https://nodejs.org/en/download/)
+Additional dependencies (in `node_modules`) were installed via the following terminal commands:
+- `npm install discord.io winston –save`
+- `npm install https://github.com/woor/discord.io/tarball/gateway_v6`
---
+++ src/main.js
@@ -0,0 +1,45 @@
+// code heavily based on the following source:
+// https://www.digitaltrends.com/gaming/how-to-make-a-discord-bot/
+
+var rootDir = "./..";
+var node_modules_dir = rootDir + "/node_modules"
+
+var Discord = require(node_modules_dir + '/discord.io');
+var logger = require(node_modules_dir + '/winston');
+var auth = require(rootDir + '/json/secure/auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
