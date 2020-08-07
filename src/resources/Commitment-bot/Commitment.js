cf40eea49a4d38d8d045138d8441663e4ade866b
Jacob Stavrianos <jacobstavrianos@gmail.com>
2020-08-06 20:42:12 -0400

    starting to develop Commitment-bot resources

---
+++ src/resources/Commitment-bot/Commitment.js
@@ -0,0 +1,37 @@
+class Commitment {
+
+  constructor(bot, user,, channelID, comJSON) {
+    // TODO: fill in
+
+    // info about bot, to return messages
+    this.bot = bot;
+    this.channelID = channelID;
+
+    // info about commitment
+    this.user = user;
+    this.extractJSON(comJSON)
+  }
+
+  extractJSON(comJSON) {
+
+    this.name = comJSON['name'];
+    this.period = time2hours(comJSON['period']);
+  }
+
+  // checks whether the commitment was fulfilled
+  checkFulfillment() {
+    if (! this.fulfilled) {
+      onFailure();
+    }
+    this.fulfulled = false;
+  }
+
+  // determines reaction based on whether commitment is fulfilled
+  onSuccess() {
+    bot.say(channelID, "good job m8");
+  }
+  onFailure() {
+    boy.say(channelID, "B A d DO THE THING");
+  }
+
+}
